7044a6ad0a151cb2e8a6f3b09b000faa275b3f7b
Bastien Guyl <bguyl@smart-tale.com>
2021-01-06 08:47:30 +0100

    Working on VoidEvent

---
+++ Assets/FoobarAction.cs
@@ -0,0 +1,8 @@
+﻿[UnityEngine.CreateAssetMenuAttribute(menuName = "Unity Atoms/Examples/Health Logger")]
+public class HealthLogger : UnityAtoms.BaseAtoms.IntAction
+{
+	public override void Do(int health)
+	{
+		UnityEngine.Debug.Log("<3: " + health);
+	}
+}
---
+++ Assets/MultipleOf.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityAtoms;
+using UnityAtoms.BaseAtoms;
+
+// Set the icon you will see in the editor
+[EditorIcon("atom-icon-sand")]
+
+// Set the path in the asset creation menu
+[CreateAssetMenu(menuName = "Unity Atoms/Conditions/Int/MultipleOf", fileName = "MultipleOf")]
+
+public class MultipleOf : FloatCondition
+{
+	// Can be set via the Inspector
+	public int multiple;
+
+	public override bool Call(float value)
+	{
+		// The condition implementation must return a boolean value
+
+		return value % multiple == 0;
+	}
+}
