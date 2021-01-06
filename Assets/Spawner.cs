946cd46339c11c08670edc3c3e05928760f43027
Bastien Guyl <bguyl@smart-tale.com>
2021-01-06 08:46:54 +0100

    1st working version of VoidEvent

---
+++ Assets/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public int nbInstances = 1;
+	public GameObject prefab;
+
+	void Start()
+	{
+		for (int i = 0; i < nbInstances; i++)
+		{
+			Instantiate(prefab);
+		}
+	}
+}
